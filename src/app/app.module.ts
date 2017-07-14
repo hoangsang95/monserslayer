7c514e935253bbed6af37c31badc1c97495f9527
Hoang Sang <sangdlh95@gmail.com>
2017-07-14 23:24:32 +0700

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MonsterAttackComponentComponent } from './monster-attack-component/monster-attack-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonsterAttackComponentComponent
   ],
   imports: [
     BrowserModule
